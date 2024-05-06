eb09fb1580463a11c60a2a0d7809127e91de0e91
Axel <axel.lopezs@alumno.buap.mx>
2024-05-06 16:42:42 -0600

    third commit

--- src/components/GaleriaScreen.jsx
+++ src/components/GaleriaScreen.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet, ScrollView, Dimensions, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { View, Text, Image, StyleSheet, ScrollView, Dimensions } from 'react-native';
 import AppBar from './AppBar';
 
 const GaleriaScreen = ({ navigation }) => {
@@ -24,22 +24,9 @@ const GaleriaScreen = ({ navigation }) => {
         require('./assets/Diecinueve.jpg'),
     ];
 
-    // Dimensiones de la ventana
-    const windowWidth = Dimensions.get('window').width;
-    const imageWidth = (windowWidth - 60) / 2;
-
-    // Estados para controlar la visualización ampliada de las imágenes
-    const [selectedImageIndex, setSelectedImageIndex] = useState(null);
-
-    // Función para manejar el toque en una imagen
-    const handleImagePress = (index) => {
-        setSelectedImageIndex(index);
-    };
-
-    // Función para manejar el cierre de la visualización ampliada de la imagen
-    const handleCloseImage = () => {
-        setSelectedImageIndex(null);
-    };
+    // Divide las imágenes en dos columnas
+    const column1 = imagePaths.slice(0, Math.ceil(imagePaths.length / 2));
+    const column2 = imagePaths.slice(Math.ceil(imagePaths.length / 2));
 
     return (
         <View style={styles.container}>
@@ -47,40 +34,25 @@ const GaleriaScreen = ({ navigation }) => {
             <ScrollView contentContainerStyle={styles.scrollViewContainer}>
                 <Text style={styles.text}>Galería</Text>
                 <View style={styles.imageContainer}>
-                    {imagePaths.map((imagePath, index) => (
-                        <TouchableOpacity
-                            key={index}
-                            onPress={() => handleImagePress(index)}
-                            activeOpacity={0.8}
-                        >
-                            <Image
-                                source={imagePath}
-                                style={[
-                                    styles.image,
-                                    selectedImageIndex === index && styles.selectedImage,
-                                ]}
-                            />
-                        </TouchableOpacity>
-                    ))}
+                    <View style={styles.column}>
+                        {column1.map((imagePath, index) => (
+                            <Image key={index} source={imagePath} style={styles.image} />
+                        ))}
+                    </View>
+                    <View style={styles.separator} />
+                    <View style={styles.column}>
+                        {column2.map((imagePath, index) => (
+                            <Image key={index} source={imagePath} style={styles.image} />
+                        ))}
+                    </View>
                 </View>
             </ScrollView>
-            {/* Vista ampliada de la imagen seleccionada */}
-            {selectedImageIndex !== null && (
-                <TouchableOpacity
-                    style={styles.overlay}
-                    onPress={handleCloseImage}
-                    activeOpacity={1}
-                >
-                    <Image
-                        source={imagePaths[selectedImageIndex]}
-                        style={styles.enlargedImage}
-                        resizeMode="contain"
-                    />
-                </TouchableOpacity>
-            )}
         </View>
     );
 };
+
+const windowWidth = Dimensions.get('window').width;
+const imageWidth = (windowWidth - 60) / 2;
 
 const styles = StyleSheet.create({
     container: {
@@ -98,33 +70,22 @@ const styles = StyleSheet.create({
     },
     imageContainer: {
         flexDirection: 'row',
-        flexWrap: 'wrap',
         paddingHorizontal: 20,
-        justifyContent: 'space-between',
+        paddingBottom: 20,
+        justifyContent: 'center',
+    },
+    column: {
+        alignItems: 'center',
+    },
+    separator: {
+        width: 20,
     },
     image: {
         width: imageWidth,
         height: imageWidth * 0.75,
+        resizeMode: 'cover',
         marginBottom: 20,
         borderRadius: 10,
-    },
-    selectedImage: {
-        transform: [{ scale: 1.2 }],
-        zIndex: 1,
-    },
-    overlay: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        backgroundColor: 'rgba(0, 0, 0, 0.7)',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    enlargedImage: {
-        width: '90%',
-        height: '90%',
     },
 });
 
